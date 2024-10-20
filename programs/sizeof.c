538e4f9d247f8a971c0fa63a148ee2158ad1b2ec
xeylou <71969414+xeylou@users.noreply.github.com>
2024-10-20 02:41:08 +0200

    feat: add meaning of ld in sizeof file

--- programs/sizeof.c
+++ programs/sizeof.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// ld => long int
+
 int main() {
     printf("%ld octets sont utilisés pour un entier\n", sizeof(int));
     printf("%ld octets sont utilisés pour un float\n", sizeof(float));
