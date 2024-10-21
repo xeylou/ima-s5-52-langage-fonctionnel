7d8527156b4ad18afb7251795398a3336d4c6742
xeylou <71969414+xeylou@users.noreply.github.com>
2024-10-21 14:26:34 +0200

    feat: exercice 1 but average 3 instead of 3.33

--- programs/tp2/exo1.c
+++ programs/tp2/exo1.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    
+    int counter = 0, input_int, total = 0;
+    float average;
+    while (1) {
+        scanf("%d", &input_int);
+        if (input_int == -1) {
+            break;
+        }
+        else {
+            counter ++;
+            total += input_int;
+        }
+        //printf("res: %d", counter);
+    }
+    if (counter>1) {
+        average = total / counter;
+        printf("%d total\n", total);
+        printf("%d counter\n", counter);
+        printf("%f average", average);
+    }
+    return 0;
 }
